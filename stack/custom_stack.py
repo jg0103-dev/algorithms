c608e56949a6c0608272e4bfc6139364aa5c436a
jg0103-dev <jgryu0103@gmail.com>
2019-12-08 22:14:15 +0900

    update

--- stack/custom_stack.py
+++ stack/custom_stack.py
@@ -1,8 +1,3 @@
-import sys
-
-t_idx = 0
-v_idx = 1
-MAX_SIZE = 100000
 
 
 class Stack:
@@ -58,33 +53,3 @@ class Stack:
             return -1
 
         return self._stack_arr[self._top]
-
-
-def main():
-
-    lines = sys.stdin.readline()
-    lines = int(lines)
-    stack = Stack(MAX_SIZE)
-
-    for line in range(lines):
-        each_line = sys.stdin.readline().split()
-        stack_type = each_line[t_idx]
-
-        if stack_type == 'push':
-            stack.push(int(each_line[v_idx]))
-
-        if stack_type == 'pop':
-            print(stack.pop())
-
-        if stack_type == 'size':
-            print(stack.size())
-
-        if stack_type == 'empty':
-            print(stack.empty())
-
-        if stack_type == 'top':
-            print(stack.top())
-
-
-if __name__ == "__main__":
-    main()
